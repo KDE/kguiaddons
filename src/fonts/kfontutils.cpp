9625907871b37e400f22bcd38aa7228e802e027b
Albert Astals Cid <aacid@kde.org>
2014-09-18 23:42:55 +0200

    Make KFontUtils::adaptFontSize be a bit more exact
    Not all fonts are linear in drawn size against their point size, the old code assumes so and simply guesses the next font size taking into account the pointSize we drawn, the geometry we got and the geometry we want. The new code is a bit slower but will make sure that we get better pointSize results in non linear fonts.
    
    REVIEW: 119777

--- src/fonts/kfontutils.cpp
+++ src/fonts/kfontutils.cpp
@@ -1,6 +1,6 @@
 /*********************************************************************************
  *                                                                               *
- *   Copyright (C) 2005, 2009 by Albert Astals Cid <aacid@kde.org>               *
+ * Copyright (C) 2005, 2009, 2014 by Albert Astals Cid <aacid@kde.org>           *
  *                                                                               *
  * This library is free software; you can redistribute it and/or                 *
  * modify it under the terms of the GNU Lesser General Public                    *
@@ -22,33 +22,68 @@
 
 #include "kfontutils.h"
 
+#include <qmath.h>
 #include <qpainter.h>
+
+static bool checkFits(QPainter &painter, const QString &string, qreal width, qreal height, qreal size, KFontUtils::AdaptFontSizeOptions flags)
+{
+    QFont f = painter.font();
+    f.setPointSizeF(size);
+    painter.setFont(f);
+    int qtFlags = Qt::AlignCenter | Qt::TextWordWrap;
+    if (flags & KFontUtils::DoNotAllowWordWrap) {
+        qtFlags &= ~Qt::TextWordWrap;
+    }
+    const QRectF boundingRect = painter.boundingRect(QRectF(0, 0, width, height), qtFlags, string);
+    if (boundingRect.width() == 0 || boundingRect.height() == 0) {
+        return false;
+    } else if (boundingRect.width() > width || boundingRect.height() > height) {
+        return false;
+    }
+    return true;
+}
 
 qreal KFontUtils::adaptFontSize(QPainter &painter, const QString &string, qreal width, qreal height, qreal maxFontSize, qreal minFontSize, AdaptFontSizeOptions flags)
 {
-    qreal size = maxFontSize;
-    QRectF boundingRect;
-    bool done = false;
+    // A invalid range is an error (-1)
+    if (maxFontSize < minFontSize)
+        return -1;
 
-    while (!done && size > minFontSize) {
-        QFont f = painter.font();
-        f.setPointSizeF(size);
-        painter.setFont(f);
-        int qtFlags = Qt::AlignCenter | Qt::TextWordWrap;
-        if (flags & DoNotAllowWordWrap) {
-            qtFlags = qtFlags & ~Qt::TextWordWrap;
-        }
-        boundingRect = painter.boundingRect(QRectF(0, 0, width, height), qtFlags, string);
-        if (boundingRect.width() == 0 || boundingRect.height() == 0) {
+    // If the max font size already fits, return it
+    if (checkFits(painter, string, width, height, maxFontSize, flags))
+        return maxFontSize;
+
+    qreal fontSizeDoesNotFit = maxFontSize;
+
+    // If the min font size does not fit, try to see if a font size of 1 fits,
+    // if it does not return error (-1)
+    // if it does, we'll return a fontsize smaller than the minFontSize as documented
+    if (!checkFits(painter, string, width, height, minFontSize, flags)) {
+        fontSizeDoesNotFit = minFontSize;
+
+        minFontSize = 1;
+        if (!checkFits(painter, string, width, height, minFontSize, flags))
             return -1;
-        } else if (boundingRect.width() > width || boundingRect.height() > height) {
-            size = qMin(width * size / boundingRect.width(), height * size / boundingRect.height());
+    }
+
+    qreal fontSizeFits = minFontSize;
+    qreal nextFontSizeToTry = (fontSizeDoesNotFit + fontSizeFits) / 2;
+
+    while (qFloor(fontSizeFits) != qFloor(nextFontSizeToTry)) {
+        if (checkFits(painter, string, width, height, nextFontSizeToTry, flags)) {
+            fontSizeFits = nextFontSizeToTry;
+            nextFontSizeToTry = (fontSizeDoesNotFit + fontSizeFits) / 2;
         } else {
-            done = true;
+            fontSizeDoesNotFit = nextFontSizeToTry;
+            nextFontSizeToTry = (nextFontSizeToTry + fontSizeFits) / 2;
         }
     }
 
-    return size;
+    QFont f = painter.font();
+    f.setPointSizeF(fontSizeFits);
+    painter.setFont(f);
+
+    return fontSizeFits;
 }
 
 qreal KFontUtils::adaptFontSize(QPainter &painter, const QString &text, const QSizeF &availableSize, qreal maxFontSize, qreal minFontSize, AdaptFontSizeOptions flags)
