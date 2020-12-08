aa2215e85ba0b4959229bc7d294f0c245129bdfc
Nicolas Fella <nicolas.fella@gmx.de>
2020-12-08 13:11:00 +0100

    Use | instead of + for combining
    Using operator+ for QFlags is disabled in Qt6

--- autotests/keysequencerecordertest.cpp
+++ autotests/keysequencerecordertest.cpp
@@ -54,15 +54,15 @@ void KeySequenceRecorderTest::testRecording()
     QCOMPARE(recordingSpy.count(), 1);
     QCOMPARE(sequenceSpy.count(), 4); // two key events + modifier release
     QCOMPARE(resultSpy.count(), 0);
-    QCOMPARE(recorder.currentKeySequence(), QKeySequence(Qt::Key_A + Qt::ControlModifier));
+    QCOMPARE(recorder.currentKeySequence(), QKeySequence(Qt::Key_A | Qt::ControlModifier));
 
     QTest::qWait(800);
     QCOMPARE(sequenceSpy.count(), 4);
     QCOMPARE(recordingSpy.count(), 2);
     QVERIFY(!recorder.isRecording());
     QCOMPARE(resultSpy.count(), 1);
-    QCOMPARE(recorder.currentKeySequence(), QKeySequence(Qt::Key_A + Qt::ControlModifier));
-    QCOMPARE(resultSpy.takeFirst().at(0).value<QKeySequence>(), QKeySequence(Qt::Key_A + Qt::ControlModifier));
+    QCOMPARE(recorder.currentKeySequence(), QKeySequence(Qt::Key_A | Qt::ControlModifier));
+    QCOMPARE(resultSpy.takeFirst().at(0).value<QKeySequence>(), QKeySequence(Qt::Key_A | Qt::ControlModifier));
 }
 
 void KeySequenceRecorderTest::testModifiers()
@@ -98,7 +98,7 @@ void KeySequenceRecorderTest::testModifiers()
 
     QTest::keyPress(m_window, Qt::Key_Alt, Qt::ControlModifier);
     QCOMPARE(sequenceSpy.count(), 6); // QTest sends two key events, one for each modifier
-    QCOMPARE(recorder.currentKeySequence(), QKeySequence(Qt::AltModifier + Qt::ControlModifier));
+    QCOMPARE(recorder.currentKeySequence(), QKeySequence(Qt::AltModifier | Qt::ControlModifier));
 
     QCOMPARE(resultSpy.count(), 0); //modifierless not allowed
 }
@@ -144,7 +144,7 @@ void KeySequenceRecorderTest::testMultiKeyAllowed()
         QVERIFY(recorder.isRecording());
         QCOMPARE(recordingSpy.count(), 1);
         QCOMPARE(resultSpy.count(), 0);
-        keys[i] = Qt::Key_A + Qt::ControlModifier;
+        keys[i] = Qt::Key_A | Qt::ControlModifier;
         QKeySequence result(keys[0], keys[1], keys[2], keys[3]);
         QTest::keyPress(m_window, Qt::Key_A, Qt::ControlModifier);
         QTest::keyRelease(m_window, Qt::Key_A, Qt::ControlModifier);
@@ -160,9 +160,9 @@ void KeySequenceRecorderTest::testMultiKeyAllowed()
     QVERIFY(recorder.isRecording());
     QCOMPARE(recordingSpy.count(), 3);
     QTest::keyPress(m_window, Qt::Key_A, Qt::ControlModifier);
-    QCOMPARE(recorder.currentKeySequence(), QKeySequence(Qt::Key_A + Qt::ControlModifier));
+    QCOMPARE(recorder.currentKeySequence(), QKeySequence(Qt::Key_A | Qt::ControlModifier));
     QVERIFY(!recorder.isRecording());
     QCOMPARE(recordingSpy.count(), 4);
     QCOMPARE(resultSpy.count(), 1);
-    QCOMPARE(resultSpy.takeAt(0).at(0).value<QKeySequence>(), QKeySequence(Qt::Key_A + Qt::ControlModifier));
+    QCOMPARE(resultSpy.takeAt(0).at(0).value<QKeySequence>(), QKeySequence(Qt::Key_A | Qt::ControlModifier));
 }
