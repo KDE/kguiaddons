22c428e9a35b7bd46835179ba44616d05795597e
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2021-09-19 09:16:35 +0200

    Relicense remaining files away from LGPL-2.0-only
    License change is approved by private mail with Michael Pyne (stored
    in sysadmin archive) as well as with relicensecheck for the remaining
    edits of the file without explicit copyright statements.

--- src/util/klocalimagecacheimpl.cpp
+++ src/util/klocalimagecacheimpl.cpp
@@ -1,7 +1,7 @@
 /*  This file is part of the KDE project.
     SPDX-FileCopyrightText: 2010 Michael Pyne <mpyne@kde.org>
 
-    SPDX-License-Identifier: LGPL-2.0-only
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
 
 #include "klocalimagecacheimpl.h"
