a0a118111c5d1ef913e630780b64bfa39cf214e2
Georg Gadinger <nilsding@nilsding.org>
2023-01-06 09:19:16 +0100

    KColorSchemeWatcherMac: do not use new NSAppearance API on older macOS versions
    On macOS versions older than Big Sur (11.0) the
    `NSAppearance.currentDrawingAppearance` property does not exist, and
    trying to use it will crash the app at runtime.
    
    This commit makes sure the new property is used on Big Sur or newer
    macOS releases, and uses the deprecated `NSAppearance.currentAppearance`
    property on Mojave (10.14) or newer.  As the light/dark mode switch was
    introduced in Mojave, running the app on older releases will always
    prefer the light mode.
    
    Tested on macOS 10.15.5 (x86_64) and 13.1 (arm64)
    
    BUG: 463752
    (cherry picked from commit c28c8b7442cc798d09239c0cf72b741d3e01fee6)

--- src/colors/kcolorschemewatcher_mac.mm
+++ src/colors/kcolorschemewatcher_mac.mm
@@ -34,5 +34,16 @@ KColorSchemeWatcherMac::~KColorSchemeWatcherMac()
 
 KColorSchemeWatcher::ColorPreference KColorSchemeWatcherMac::systemPreference() const
 {
-    return NSAppearance.currentDrawingAppearance.name == NSAppearanceNameDarkAqua ? KColorSchemeWatcher::PreferDark : KColorSchemeWatcher::PreferLight;
+    NSAppearance *appearance = nullptr;
+
+    if (@available(macOS 11.0, *)) {
+        appearance = NSAppearance.currentDrawingAppearance;
+    } else if (@available(macOS 10.14, *)) {
+        appearance = NSAppearance.currentAppearance;
+    } else {
+        // macOS < 10.14 (Mojave) does not support a light/dark mode switch, always prefer light mode
+        return KColorSchemeWatcher::PreferLight;
+    }
+
+    return appearance.name == NSAppearanceNameDarkAqua ? KColorSchemeWatcher::PreferDark : KColorSchemeWatcher::PreferLight;
 }
