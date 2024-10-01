9078617324c259f20168365fb0a721f2cb416220
Carl Schwan <carl@carlschwan.eu>
2024-10-01 14:00:01 +0200

    Add QQuickImageProvider for blurhash

---
+++ src/qml/blurhashimageprovider.h
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2024 Joshua Goins <josh@redstrate.com>
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <QQuickAsyncImageProvider>
+#include <QThreadPool>
+
+class AsyncImageResponse final : public QQuickImageResponse
+{
+public:
+    AsyncImageResponse(const QString &id, const QSize &requestedSize, QThreadPool *pool);
+    void handleDone(QImage image);
+    [[nodiscard]] QQuickTextureFactory *textureFactory() const override;
+    QImage m_image;
+};
+
+class BlurHashImageProvider : public QQuickAsyncImageProvider
+{
+public:
+    QQuickImageResponse *requestImageResponse(const QString &id, const QSize &requestedSize) override;
+
+private:
+    QThreadPool pool;
+};
---
+++ src/qml/privateplugin.cpp
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2024 Carl Schwan <carl@carlschwan.eu>
+// SPDX-License-Identifier: LGPL-2.1-or-later
+//
+#include "privateplugin.h"
+#include "blurhashimageprovider.h"
+
+void PrivatePlugin::registerTypes(const char *uri)
+{
+    Q_UNUSED(uri);
+}
+
+void PrivatePlugin::initializeEngine(QQmlEngine *engine, const char *)
+{
+    engine->addImageProvider(QLatin1StringView("blurhash"), new BlurHashImageProvider);
+}
